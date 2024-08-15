a8fd105e523880aebe2f21e3ceb861175b0fea07
rupesh <rpiwal02@gmail.com>
2024-08-16 00:13:06 +0530

    items list rendered

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import JobListItem from "@/components/JobListItem";
+import prisma from "@/lib/prisma";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const jobs = await prisma.job.findMany({
+    where: { approved: true },
+    orderBy: { createdAt: "desc" },
+  });
   return (
     <div>
-      <JobListItem />
+      {jobs.map((job) => (
+        <JobListItem key={job.id} job={job} />
+      ))}
     </div>
   );
 }
--- src/components/JobListItem.tsx
+++ src/components/JobListItem.tsx
@@ -1,5 +1,37 @@
-const JobListItem = () => {
-  return <div>JobListItem</div>;
+import { Job } from "@prisma/client";
+import Image from "next/image";
+import { Badge } from "./ui/badge";
+
+interface JobListItemProps {
+  job: Job;
+}
+
+const JobListItem = async ({
+  job: {
+    title,
+    type,
+    locationType,
+    location,
+    description,
+    companyName,
+    applicationEmail,
+    applicationUrl,
+    companyLogoUrl,
+  },
+}: JobListItemProps) => {
+  return (
+    <div className="max-w-[919px] border border-gray-200 py-4">
+      <div>{companyLogoUrl}</div>
+      <div className="flex flex-col  justify-start gap-2 pl-10">
+        <div className="font-semibold">{title}</div>
+        <div className="text-sm text-gray-500">{companyName}</div>
+        <div>
+          <Badge variant="secondary">{type}</Badge>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default JobListItem;
