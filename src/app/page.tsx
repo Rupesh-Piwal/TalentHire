6ab9a58f1f2afa3a26735c03ada2016467eb6a61
rupesh <rpiwal02@gmail.com>
2024-08-16 13:02:45 +0530

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,20 @@ export default async function Home() {
     orderBy: { createdAt: "desc" },
   });
   return (
-    <div className="m-auto min-h-full max-w-5xl px-3">
-      {jobs.map((job) => (
-        <JobListItem key={job.id} job={job} />
-      ))}
-    </div>
+    <main className="m-auto min-h-full max-w-5xl px-3">
+      <div className=" border-2 border-gray-800 py-10 text-center text-[48px] font-semibold">
+        <h1 className="font-clash-display text-xl">
+          Find your <span>dream job</span>
+        </h1>
+        <p></p>
+      </div>
+      <section>
+        <div>
+          {jobs.map((job) => (
+            <JobListItem key={job.id} job={job} />
+          ))}
+        </div>
+      </section>
+    </main>
   );
 }
