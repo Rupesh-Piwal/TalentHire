8fe117a7ea4fbfabca606d01fb821711a70534f0
rupesh <rpiwal02@gmail.com>
2024-08-27 21:50:13 +0530

    removed banner search-bar

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -11,42 +11,6 @@ const Banner = () => {
           Find your next career at companies like HubSpot, Nike, and Dropbox
         </p>
       </div>
-
-      {/* For screens above md */}
-      <div className="hidden flex-col gap-3 py-4 md:flex">
-        <div className="flex items-center justify-center gap-3">
-          <Search size={26} className="text-[#515B6F]" />
-          <input
-            className="rounded px-10 py-3 outline-none md:min-w-[600px] md:px-2"
-            type="search"
-            name=""
-            id=""
-          />
-          <button className="rounded bg-[#4640DE] px-4 py-3 font-semibold text-white hover:bg-[#4640DE]/90">
-            Search
-          </button>
-        </div>
-      </div>
-
-      {/* For screens below md */}
-      <div className="mx-8 flex flex-col items-center justify-center gap-3 bg-white md:hidden">
-        <div className="flex flex-row items-center justify-between">
-          <Search size={26} className="text-[#515B6F]" />
-          <input
-            className="rounded px-6 py-3 outline-none"
-            type="search"
-            name=""
-            id=""
-          />
-        </div>
-        <button className="w-full rounded bg-[#4640DE] px-4 py-3 font-semibold text-white hover:bg-[#4640DE]/90">
-          Search my job
-        </button>
-      </div>
-
-      <p className="mt-4 px-8 text-[16px] text-[#515B6F]">
-        UI Designer, UX Researcher, Android, Admin
-      </p>
     </div>
   );
 };
