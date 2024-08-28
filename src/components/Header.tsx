70242c36994f105b61bb61d7ed0d4b7004ff52e6
rupesh <rpiwal02@gmail.com>
2024-08-28 13:11:04 +0530

    updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Logo from "@/assets/talenthire-logo.svg";
+import Logo from "@/assets/briefcase 1.svg";
 import Image from "next/image";
 import { Menu } from "lucide-react";
 
@@ -10,23 +10,27 @@ const Header = () => {
         <div className="flex min-w-[375px] flex-row items-center justify-between">
           <Link href="/" className="flex items-center gap-2">
             <Image src={Logo} alt="talenthire-logo" width={36} height={36} />
-            <div className="text-2xl font-semibold">TalentHire</div>
+            <div className="text-2xl font-semibold text-[#18191C]">
+              TalentHire
+            </div>
           </Link>
           <Menu size={20} className="md:hidden" />
         </div>
 
         <div className="hidden gap-4 md:flex">
-          <Link className="hover:text-[#4640DE]" href="/jobs">
+          <Link className="hover:text-[#0A65CC]" href="/jobs">
             Find Jobs
           </Link>
-          <Link href="/companies">Browse Companies</Link>
+          <Link className="hover:text-[#0A65CC]" href="/companies">
+            Browse Companies
+          </Link>
         </div>
       </div>
       <div className="hidden gap-4 md:flex">
-        <button className="rounded bg-gray-100 px-4 py-2 font-semibold text-[#4640DE] shadow-sm hover:bg-gray-200">
+        <button className="rounded bg-gray-100 px-4 py-2 font-semibold text-[#0A65CC] shadow-sm hover:bg-gray-200">
           Login
         </button>
-        <button className="rounded bg-[#4640DE] px-4 py-2 font-semibold text-white hover:bg-[#4640DE]/90">
+        <button className="rounded bg-[#0A65CC] px-4 py-2 font-semibold text-white hover:bg-[#0A65CC]/90">
           Post Jobs
         </button>
       </div>
