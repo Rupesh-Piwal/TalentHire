ef008e311d96705c4d94ec151591de9aaf7851bf
rupesh <rpiwal02@gmail.com>
2024-09-22 11:03:00 +0530

    undo changes

--- src/app/jobs/new/page.tsx
+++ src/app/jobs/new/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-// import NewJobForm from "./NewJobForm";
+import NewJobForm from "./NewJobForm";
 
 export const metadata: Metadata = {
   title: "Post a new job",
