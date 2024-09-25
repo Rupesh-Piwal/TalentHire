01447b16f3f7bac4a151b0d924e959c04cf5cbcd
rupesh <rpiwal02@gmail.com>
2024-09-25 09:32:04 +0530

    job submit page

---
+++ src/app/job-submitted/page.tsx
@@ -0,0 +1,10 @@
+const page = () => {
+  return (
+    <main className="m-auto my-10 max-w-5xl space-y-5 px-3 text-center">
+      <h2>Job submitted</h2>
+      <p>Your job posting has been submitted and is pending approval.</p>
+    </main>
+  );
+};
+
+export default page;
