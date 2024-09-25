4933cbf945a3d874148b7fef7f39506e45787696
rupesh <rpiwal02@gmail.com>
2024-09-25 12:45:17 +0530

    incorporated pulsing-button in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,33 @@
 import Link from "next/link";
 import Logo from "@/assets/briefcase 1.svg";
 import Image from "next/image";
-import { Menu } from "lucide-react";
+import PulsatingButton from "./magicui/pulsating-button";
 
 const Header = () => {
   return (
     <div className="w-scren flex h-[78px] items-center justify-between px-[20px] shadow-md  md:px-[150px]">
       <div className="flex items-center gap-8">
-        <div className="flex min-w-[375px] flex-row items-center justify-between">
+        <div className="flex flex-row items-center justify-between md:min-w-[375px]">
           <Link href="/" className="flex items-center gap-2">
             <Image src={Logo} alt="talenthire-logo" width={36} height={36} />
-            <div className="text-2xl font-semibold text-[#18191C]">
+            <div className="text-xl font-semibold text-[#18191C] md:text-2xl">
               TalentHire
             </div>
           </Link>
-          <Menu size={20} className="md:hidden" />
         </div>
       </div>
-      <div className="hidden gap-4 md:flex">
-        <button className="rounded bg-gray-100 px-4 py-2 font-semibold text-[#0A65CC] shadow-sm hover:bg-gray-200">
-          Hire Me
-        </button>
+      <div className=" flex gap-4">
+        <PulsatingButton>
+          <Link
+            href="https://drive.google.com/file/d/1xvJMhNfpH3jzZaXXPClHjaATno0PRefg/view?usp=sharing"
+            target="_blank"
+          >
+            Hire Me
+          </Link>
+        </PulsatingButton>
         <Link
           href="/jobs/new"
-          className="rounded bg-[#0A65CC] px-4 py-2 font-semibold text-white hover:bg-[#0A65CC]/90"
+          className="rounded border border-[#0A65CC] px-4 py-2 font-semibold text-[#0A65CC] hover:bg-[#0A65CC]/90"
         >
           Post Jobs
         </Link>
