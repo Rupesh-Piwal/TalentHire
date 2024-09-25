966046a7104ea109a0c478f1356c61cca638a955
rupesh <rpiwal02@gmail.com>
2024-09-25 10:22:55 +0530

    typography changes

--- src/app/job-submitted/page.tsx
+++ src/app/job-submitted/page.tsx
@@ -1,8 +1,10 @@
 const page = () => {
   return (
-    <main className="m-auto my-10 max-w-5xl space-y-5 px-3 text-center">
-      <h2>Job submitted</h2>
-      <p>Your job posting has been submitted and is pending approval.</p>
+    <main className="m-auto my-10 flex max-w-5xl flex-col items-center justify-center space-y-5 px-3 text-center">
+      <h2 className="text-3xl font-semibold text-[#0A65CC]">Job submitted</h2>
+      <p className="text-slate-700">
+        Your job posting has been submitted and is pending approval.
+      </p>
     </main>
   );
 };
