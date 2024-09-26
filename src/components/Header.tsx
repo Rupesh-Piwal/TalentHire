dcefdfe6bae05ccc4cf1d7b8751ebff04381f9dc
rupesh <rpiwal02@gmail.com>
2024-09-26 12:32:19 +0530

    mobile responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,18 +5,18 @@ import PulsatingButton from "./magicui/pulsating-button";
 
 const Header = () => {
   return (
-    <div className="w-scren flex h-[78px] items-center justify-between px-[20px] shadow-md  md:px-[150px]">
-      <div className="flex items-center gap-8">
+    <div className="w-scren flex h-[78px] items-center justify-between px-[18px] shadow-md  md:px-[150px]">
+      <div className="flex items-center gap-4">
         <div className="flex flex-row items-center justify-between md:min-w-[375px]">
           <Link href="/" className="flex items-center gap-2">
             <Image src={Logo} alt="talenthire-logo" width={36} height={36} />
-            <div className="text-xl font-semibold text-[#18191C] md:text-2xl">
+            <div className="text-[14px] font-semibold text-[#18191C] md:text-2xl">
               TalentHire
             </div>
           </Link>
         </div>
       </div>
-      <div className=" flex gap-4">
+      <div className=" flex gap-3">
         <PulsatingButton>
           <Link
             href="https://drive.google.com/file/d/1xvJMhNfpH3jzZaXXPClHjaATno0PRefg/view?usp=sharing"
@@ -27,7 +27,7 @@ const Header = () => {
         </PulsatingButton>
         <Link
           href="/jobs/new"
-          className="rounded border border-[#0A65CC] px-4 py-2 font-semibold text-[#0A65CC] hover:bg-[#0A65CC]/90"
+          className="rounded-sm border border-[#0A65CC] px-[4px] py-2 text-center text-[15px] font-semibold text-[#0A65CC] hover:bg-[#0A65CC]/90 md:px-4"
         >
           Post Jobs
         </Link>
