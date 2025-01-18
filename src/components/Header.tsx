182d7706069a78084715e771a6bf47394cf1f589
rupesh <rpiwal02@gmail.com>
2025-01-18 18:21:52 +0530

    minor chnages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -49,7 +49,7 @@ const Header = () => {
           <div className="flex gap-3">
             <PulsatingButton>
               <Link
-                href="https://drive.google.com/file/d/196oEqzqVt_zX5Wj4nyYTqSDRBeAvAFKS/view?usp=sharing"
+                href="https://drive.google.com/file/d/1T5wvH500lidVkPbwOqoLEce-8FEHjzns/view?usp=sharing"
                 target="_blank"
               >
                 Hire Me
